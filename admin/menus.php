d108b8a4583bfed81fec47fb9e9290bfcf21c3b9
kazuha <86008814+embedstreamenime@users.noreply.github.com>
2021-11-26 03:52:03 +0700

    Create menus.php

---
+++ admin/menus.php
@@ -0,0 +1,20 @@
+<div class="list-group" style="border-radius: 0 !important;">
+    <a href="/admin/remote-files" class="list-group-item list-group-item-action<?= $menu_active == '1' ? ' active':'';?> no-border">
+        <i class="fa fa-file fa-fw"></i> Remote Files
+    </a>
+    <a href="/admin/lastdls" class="list-group-item list-group-item-action<?= $menu_active == '2' ? ' active':'';?> no-border">
+        <i class="fa fa-download fa-fw"></i> Last Downloads
+    </a>
+    <a href="/admin/most-downloads" class="list-group-item list-group-item-action<?= $menu_active == '3' ? ' active':'';?> no-border">
+        <i class="fa fa-cloud-download fa-fw"></i> Most Downloads
+    </a>
+    <a href="/admin/last-registered" class="list-group-item list-group-item-action<?= $menu_active == '4' ? ' active':'';?> no-border">
+        <i class="fa fa-users fa-fw"></i> Last User Registered
+    </a>
+    <a href="/admin/top-users" class="list-group-item list-group-item-action<?= $menu_active == '5' ? ' active':'';?> no-border">
+        <i class="fa fa-star fa-fw"></i> Top Users
+    </a>
+    <a href="/admin/mirror-settings" class="list-group-item list-group-item-action<?= $menu_active == '6' ? ' active':'';?> no-border">
+        <i class="fa fa-external-link fa-fw"></i> Mirror Account
+    </a>
+</div>
